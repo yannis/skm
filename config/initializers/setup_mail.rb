a7e27440881d07a8b7054dbc92465e0c97e704a7
Yannis Jaquet <yannis.jaquet@unige.ch>
2015-08-14 09:08:03 +0200

    changes translation a bit

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "heroku.com",
+  domain: "herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV['GMAIL_USERNAME'],
