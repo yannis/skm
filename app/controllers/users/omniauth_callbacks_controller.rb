84bd09ee2ba87579f250003fc9f21ea6a4c93c68
Yannis Jaquet <yannisjaquet@mac.com>
2017-09-13 17:52:34 +0200

    Fix facebook auth bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
     if @user.persisted?
       sign_in @user, event: :authentication #this will throw if @user is not activated
